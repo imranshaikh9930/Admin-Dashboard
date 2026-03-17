46e92609dd5e78556949714d6a39435abced1d90
imranshaikh9930 <imranpathan7040@gmail.com>
2026-03-17 11:35:24 +0530

    Routes Changes Updated

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,7 +3,7 @@ import Login from './component/Login'
 import './App.css'
 import Dashboard from './Layout/Dashboard'
 import Cards from './component/Cards';
-import { BrowserRouter as Router,Routes,Route } from 'react-router-dom';
+import { BrowserRouter as Router,Routes,Route, Navigate } from 'react-router-dom';
 import Overview from './Pages/Overview';
 import Users from "./Pages/Users";
 import Booking from "./Pages/Booking";
@@ -18,8 +18,9 @@ function App() {
     <Router>
 
       <Routes>
-          <Route path="/" element={<Dashboard/>}>
-               <Route path="overview" index element={<Overview />} />
+        <Route path="/" element={<Navigate to="/dashboard" replace />} />
+          <Route path="/dashboard" element={<Dashboard/>}>
+               <Route index element={<Overview />} />
                <Route path="users" element={<Users />} />
                 <Route path="booking" element={<Booking />} />
                  <Route path="reports" element={<Reports />} />
--- client/src/component/Sidebar.jsx
+++ client/src/component/Sidebar.jsx
@@ -1,81 +1,4 @@
-// import { House } from "lucide-react";
-// import React, { useContext, useState } from "react";
-// // import { CgProfile } from "react-icons/cg";
-// // import { IoMdSettings } from "react-icons/io";
-// // import { Tooltip } from "@mui/material";
-// import { LayoutDashboard } from 'lucide-react';
-// import { MdDashboard } from "react-icons/md";
-// import { FaUsers } from "react-icons/fa";
 
-// import { NavLink, useNavigate } from "react-router-dom";
-
-
-//   const navclassName = ({ isActive }) =>
-//     `flex items-center gap-5 px-6 py-2 rounded transition border
-//      ${isActive
-//       ? "bg-[#222F7D] text-white"
-//       : "text-black hover:bg-[#222F7D] hover:text-white border-[#dbdbdb]"
-//     }`;
-
-// const Sidebar = () => {
-//   const [open,setOpen] = useState(false);
-
-
-//   return (
-// //     <>
-// //       {/* MOBILE OVERLAY - Only acts on mobile screens */}
-// //       {/* {open && (
-// //         <div
-// //           classNameName="fixed inset-0 bg-black/50 z-30 md:hidden transition-opacity"
-// //           onClick={() => setOpen(false)}
-// //         />
-// //       )}
-// //  {/* ${open ? "translate-x-0" : "-translate-x-full"} */}
-// //       <aside
-// //         classNameName={`
-// //           fixed inset-y-0 left-0 z-40  shadow-xl
-// //           transition-all duration-300 ease-in-out 
-// //           flex flex-col h-screen overflow-hidden 
-// //           mt-16
-        
-// //           md:static md:translate-x-0 md:w-[250px] 
-          
-// //         `}
-// //       >
-
-// //            <div classNameName="relative flex items-center justify-center w-full h-16 border-b shrink-0">
-// //          {/* Logo Container */}
-// //          <div classNameName="flex items-center justify-center">
-// //            <img 
-// //             //  src={} 
-// //              classNameName="w-[100px] h-[40px] object-contain" 
-// //              alt="Logo" 
-// //            />
-// //          </div>
-// //          </div>
-
-// //            <nav classNameName="mt-4 space-y-2 px-4 flex-1 overflow-y-auto custom-scrollbar">
-// //           {/* <h1 classNameName="text-center">Admin Dashboard</h1> */}
-// //           <NavLink to="/admin" end 
-// //           classNameName={navclassName}
-// //           >
-// //             <House size={15} classNameName="shrink-0" />
-// //             <span classNameName="text-[15px]">Overview </span>
-// //           </NavLink>
-          
-// //           </nav>
-       
-
-// //             <h1>Sidebar</h1>
-      
-// //       </aside> */}
-// //     </>
-// <></>
- 
-//   );
-// };
-
-// export default Sidebar;
 
 import { MdDashboard } from "react-icons/md";
 import { FaUsers, FaChartBar } from "react-icons/fa";
@@ -94,7 +17,7 @@ export default function Sidebar() {
       {/* Menu */}
       <nav className="flex flex-col flex-1 space-y-6 w-full mt-[50px]">
 
-        <NavLink to="overview" className="flex items-center justify-center group-hover:justify-start gap-4 p-2 rounded-lg text-gray-700 hover:bg-gray-100 dark:text-gray-200 dark:hover:bg-gray-800 transition">
+        <NavLink to="/dashboard" className="flex items-center justify-center group-hover:justify-start gap-4 p-2 rounded-lg text-gray-700 hover:bg-gray-100 dark:text-gray-200 dark:hover:bg-gray-800 transition">
           <MdDashboard size={22} />
           <span className="hidden group-hover:block">Dashboard</span>
         </NavLink>
@@ -123,9 +46,9 @@ export default function Sidebar() {
       {/* Bottom */}
       <div className="flex flex-col space-y-6 mt-10">
 
-        <a className="p-2 rounded-lg text-gray-700 hover:bg-gray-100 dark:text-gray-200 dark:hover:bg-gray-800">
+        {/* <a className="p-2 rounded-lg text-gray-700 hover:bg-gray-100 dark:text-gray-200 dark:hover:bg-gray-800">
           <IoSettingsOutline size={22} />
-        </a>
+        </a> */}
 
         
 
